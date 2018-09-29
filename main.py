66ea04581ac0f65ecf59edc3f7a582115d7503b4
Leporoni <leporoni@hotmail.com>
2018-09-29 17:19:59 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from repositorios import cliente_repository
+from entidades import cliente
+
+cliente = cliente.Cliente("Zico", 69)
+
+cliente_repository.ClienteRepository.listar_clientes()
+#cliente_repository.ClienteRepository.inserir_cliente(cliente)
+#cliente_repository.ClienteRepository.editar_cliente(2, cliente)
+#cliente_repository.ClienteRepository.remover_cliente(2)
+
+
+
---
+++ repositorios/cliente_repository.py
@@ -0,0 +1,42 @@
+from fabricas import fabrica_conexao
+
+
+class ClienteRepository():
+
+    @staticmethod
+    def listar_clientes():
+        fabrica = fabrica_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("SELECT * FROM cliente")
+            print(cursor.fetchall())
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def inserir_cliente(cliente):
+        fabrica = fabrica_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("INSERT INTO cliente (nome, idade) values(%s, %s)", (cliente.nome, cliente.idade))
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def editar_cliente(id, cliente):
+        fabrica = fabrica_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("UPDATE cliente SET nome=%(nome)s, idade=%(idade)s WHERE id=%(id)s",
+                       ({'nome': cliente.nome, 'idade': cliente.idade, 'id': id}))
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def remover_cliente(id):
+        fabrica = fabrica_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("DELETE FROM cliente WHERE id=%s", (id,))
+        finally:
+            fabrica.close()
